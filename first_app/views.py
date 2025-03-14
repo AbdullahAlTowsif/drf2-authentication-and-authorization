3a61e19c0825ca6a073d2691a1d5010c2ded9c60
AbdullahAlTowsif <abdullah.al.towsif2002@gmail.com>
2025-03-14 22:11:20 +0600

    some permission added

--- first_app/views.py
+++ first_app/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from . import models, serializers
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 
 
 class ProductViewSet(viewsets.ModelViewSet):
+    # permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticatedOrReadOnly]
     queryset = models.Product.objects.all()
     serializer_class = serializers.ProductSerializer
 
