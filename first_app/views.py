036fd6249f7a1b3ba6babcb4a77bd933ce2854f7
AbdullahAlTowsif <abdullah.al.towsif2002@gmail.com>
2025-03-22 03:03:10 +0600

    filtering without get_queryset added

--- first_app/views.py
+++ first_app/views.py
@@ -3,6 +3,7 @@ from rest_framework import viewsets
 from . import models, serializers
 from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 from . import permissions
+from django_filters.rest_framework import DjangoFilterBackend
 
 class ProductViewSet(viewsets.ModelViewSet):
     # permission_classes = [IsAuthenticated]
@@ -14,12 +15,25 @@ class ProductViewSet(viewsets.ModelViewSet):
 
 class ProductReviewViewSet(viewsets.ModelViewSet):
     permission_classes = [permissions.ReviewerOrReadOnly]
-    # queryset = models.ProductReview.objects.all()
+    queryset = models.ProductReview.objects.all()
     serializer_class = serializers.ProductReviewSerializer
-    def get_queryset(self):
-        queryset = models.ProductReview.objects.all()
-        username = self.request.query_params.get('username')
-        if username is not None:
-            # icontains means not case-sensitive
-            queryset = queryset.filter(user__username__icontains=username)
-        return queryset
+    filter_backends = [DjangoFilterBackend]
+    # filterset_fields = ['user__username']
+    filterset_fields = ['rating', 'product']
+    
+    # when we use filter_backends, filterset_fields follow below steps:
+    # 1. api will be: http://127.0.0.1:8000/reviews/?user__username=towsif
+    # http://127.0.0.1:8000/reviews/?rating=3, http://127.0.0.1:8000/reviews/?rating=3&product=2
+    # 2. use: queryset = models.ProductReview.objects.all()
+    
+    # def get_queryset(self):
+    #     queryset = models.ProductReview.objects.all()
+    #     username = self.request.query_params.get('username')
+    #     if username is not None:
+    #         # icontains means not case-sensitive
+    #         queryset = queryset.filter(user__username__icontains=username)
+    #     return queryset
+    
+    # when we use this queryset function for filter follow below steps:
+    # 1. api will be: http://127.0.0.1:8000/reviews/?username=towsif
+    # 2. don't use: queryset = models.ProductReview.objects.all()
