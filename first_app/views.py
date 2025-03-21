843cb8b111d65f9df452fd702fcc2dc1b581cc3e
AbdullahAlTowsif <abdullah.al.towsif2002@gmail.com>
2025-03-22 02:38:19 +0600

    filtering added using get_queryset

--- first_app/views.py
+++ first_app/views.py
@@ -14,5 +14,12 @@ class ProductViewSet(viewsets.ModelViewSet):
 
 class ProductReviewViewSet(viewsets.ModelViewSet):
     permission_classes = [permissions.ReviewerOrReadOnly]
-    queryset = models.ProductReview.objects.all()
+    # queryset = models.ProductReview.objects.all()
     serializer_class = serializers.ProductReviewSerializer
+    def get_queryset(self):
+        queryset = models.ProductReview.objects.all()
+        username = self.request.query_params.get('username')
+        if username is not None:
+            # icontains means not case-sensitive
+            queryset = queryset.filter(user__username__icontains=username)
+        return queryset
